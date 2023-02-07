5a5716bb598ccf70846accbdfff67998bf4816ef
Vlad <zagoskin.3@mail.ru>
2023-02-07 11:48:11 +0300

    +

--- app/main.py
+++ app/main.py
@@ -14,17 +14,6 @@ print(device)
 
 app = FastAPI()
 
-origins = [
-    "http://zagoskin.3.fvds.ru:3000",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @app.get("/")
 async def root():
